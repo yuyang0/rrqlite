f16db9c175d691fff3303510da607f45c1f2b423
YuYang <yyangplus@gmail.com>
2022-04-02 19:00:32 +0800

    add untagged attribute to Value

--- core/command/build.rs
+++ core/command/build.rs
@@ -3,26 +3,8 @@ fn main() -> Result<()> {
     let mut config = prost_build::Config::new();
     config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
     config.type_attribute(".", "#[serde(rename_all = \"camelCase\")]");
-    // config.type_attribute(
-    //     "command.parameter.Value",
-    //     "#[derive(serde::Serialize, serde::Deserialize)]",
-    // );
-    // config.type_attribute(
-    //     "command.Parameter",
-    //     "#[derive(serde::Serialize, serde::Deserialize)]",
-    // );
-    // config.type_attribute(
-    //     "command.Statment",
-    //     "#[derive(serde::Serialize, serde::Deserialize)]",
-    // );
-    // config.type_attribute(
-    //     "command.Request",
-    //     "#[derive(serde::Serialize, serde::Deserialize)]",
-    // );
-    // config.type_attribute(
-    //     "command.ExecuteRequest",
-    //     "#[derive(serde::Serialize, serde::Deserialize)]",
-    // );
+    config.type_attribute(".command.Parameter.value", "#[serde(untagged)]");
+
     config.compile_protos(&["src/command.proto"], &["src/"])?;
     Ok(())
 }
